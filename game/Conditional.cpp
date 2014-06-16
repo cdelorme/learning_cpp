25e9ba05a122eefadf9b2b11c91b3b241761135c
Casey DeLorme <casey.delorme@hudl.com>
2014-06-16 00:05:06 -0500

    added conditional path to abstract osx logic in code

---
+++ game/Conditional.cpp
@@ -0,0 +1,40 @@
+/**
+ * Conditional.cpp
+ *
+ * author: Casey DeLorme <cdelorme@gmail.com>
+ * date: 2014-6-15
+ * link: http://stackoverflow.com/questions/516200/relative-paths-not-working-in-xcode-c
+ *
+ * description:
+ *  A feeble attempt to externalize conditional logic
+ *  from the core of my code.
+ *
+ */
+
+// Apple/OS X specific code for resources path in app bundles
+#ifdef __APPLE__
+#include "CoreFoundation/CoreFoundation.h"
+#endif
+
+// local
+#include "Conditional.h"
+
+
+// constructor
+Conditional::Conditional() {}
+
+// Assure our directory is relative to data so we can access it (primarily OSX)
+Conditional::paths() {
+
+// ----------------------------------------------------------------------------
+// This makes relative paths work in C++ in Xcode by changing directory to the Resources folder inside the .app bundle
+#ifdef __APPLE__
+    CFBundleRef mainBundle = CFBundleGetMainBundle();
+    CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
+    char path[PATH_MAX];
+    CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)path, PATH_MAX);
+    CFRelease(resourcesURL);
+    chdir(path);
+#endif
+
+};
---
+++ game/Conditional.h
@@ -0,0 +1,7 @@
+
+#pragma once
+
+class Conditional {
+    public:
+        void paths();
+};
