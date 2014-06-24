980e8524afdeb10a4742a0e1756f771889f317c2
Casey DeLorme <casey.delorme@hudl.com>
2014-06-23 22:26:36 -0500

    added uninstall script for sfml

---
+++ apple/uninstall-sfml.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# as root user uninstall sfml files
+rm -rf /Library/Frameworks/SFML.framework
+rm -rf /Library/Frameworks/sfml-*
+rm -rf /Library/Frameworks/freetype.framework
+rm -rf /Library/Frameworks/sndfile.framework
+rm -rf /usr/local/lib/libsfml-*
+rm -rf /usr/local/share/SFML
+rm -rf /Library/Developer/Xcode/Templates/SFML
+rm -rf /usr/local/include/SFML
+
